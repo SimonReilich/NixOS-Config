5cb6d7d06f557adc98cb0f60a4a7f453f3371107
SimonReilich <simon.reilich@proton.me>
2025-08-19 23:40:48 +0200

    adding ocaml

--- common/vscode.nix
+++ common/vscode.nix
@@ -4,8 +4,10 @@
   users.users.simonr.packages = with pkgs; [
     (vscode-with-extensions.override {
       vscodeExtensions = with vscode-extensions; [
+        badochov.ocaml-formatter
         bbenoist.nix
         brettm12345.nixfmt-vscode
+        ocamllabs.ocaml-platform
         piousdeer.adwaita-theme
         james-yu.latex-workshop
       ];
