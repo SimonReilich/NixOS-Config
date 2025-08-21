af726c03860a2ed473990480fabe5f568151b334
SimonReilich <simon.reilich@proton.me>
2025-08-21 12:04:56 +0200

    resolved syntax error
    [nixos-generation: 34] [hostname: desktop]

--- desktop/system/user.nix
+++ desktop/system/user.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  home-manager = builtins.fetchTarball = {
+  home-manager = builtins.fetchTarball {
     url = "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
     sha256 = "026rvynmzmpigax9f8gy9z67lsl6dhzv2p6s8wz4w06v3gjvspm1";
   };
