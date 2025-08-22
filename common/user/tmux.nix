59513d42d8f4e942dd00fd65e514748a49f3ed60
SimonReilich <simon.reilich@proton.me>
2025-08-22 12:39:41 +0200

    tweaking tmux configuration
    [nixos-generation: 60] [hostname: desktop]

--- common/user/tmux.nix
+++ common/user/tmux.nix
@@ -5,12 +5,18 @@
     enable = true;
     shell = "${pkgs.bash}/bin/bash";
     sensibleOnTop = true;
+    prefix = "C-Space";
     plugins = with pkgs.tmuxPlugins; [
       better-mouse-mode
       vim-tmux-navigator
     ];
     extraConfig = ''
+      set-option -sa terminal-overrides ",xterm*:Tc"
 
+      bind -n M-H previouse-window
+      bind -n M-L next-window
+
+      set -g @plugin 'dreamsofcode-io/catppuccin-tmux'
     '';
   };
 }
