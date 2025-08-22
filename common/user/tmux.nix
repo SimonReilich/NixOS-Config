c90ea7a7aa30e3227c0709904594f828ff4b60e5
SimonReilich <simon.reilich@proton.me>
2025-08-22 12:57:49 +0200

    tweaking tmux configuration
    [nixos-generation: 66] [hostname: desktop]

--- common/user/tmux.nix
+++ common/user/tmux.nix
@@ -10,6 +10,7 @@
       better-mouse-mode
       catppuccin
       vim-tmux-navigator
+      yank
     ];
     extraConfig = ''
       set-option -sa terminal-overrides ",xterm*:Tc"
@@ -22,6 +23,15 @@
       set -g pane-base-index 1
       set-window-option -g pane-base-index 1
       set-option -g renumber-windows on
+
+      set-window-option -g mode-keys vi
+
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancle
+
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
     '';
   };
 }
