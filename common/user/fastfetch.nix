487d0c86bf7c3b68cd73eae0ed8db3b03ca94db1
SimonReilich <simon.reilich@proton.me>
2025-08-23 00:44:03 +0200

    configuring fastfetch
    [nixos-generation: 71] [hostname: desktop]

--- common/user/fastfetch.nix
+++ common/user/fastfetch.nix
@@ -14,16 +14,45 @@
       modules = [
         "OS"
         "Kernel"
+        "Init System"
+
+        "break"
+
         "Uptime"
+        "Processes"
         "Packages"
+
+        "break"
+
         "Shell"
         "Editor"
         "DE"
         "WM"
         "Terminal"
+
+        "break"
+
+        "Board"
         "CPU"
+        "CPU Cache (L1)"
+        "CPU Cache (L2)"
+        "CPU Cache (L3)"
+        "CPU Ussage"
         "GPU 1"
+        "GPU 2"
         "Memory"
+        "Disk (/)"
+
+        "break"
+
+        "Mouse"
+        "Keyboard"
+        "Display (Xiaomi Corporation 52\")"
+        "Sound"
+
+        "break"
+
+        "Date & Time"
         "Weather"
       ];
     };
