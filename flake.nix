b58248fd724c1e29a7759b79e18c2acc496af14f
SimonReilich <simon.reilich@proton.me>
2025-08-22 11:43:57 +0200

    fixed issue, where home-manager used unstable packages
    [nixos-generation: 55] [hostname: desktop]

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
