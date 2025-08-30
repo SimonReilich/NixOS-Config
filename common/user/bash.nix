dcba842b42f8e4bb7e5ebf21a3e865236f62adfa
SimonReilich <simon.reilich@proton.me>
2025-08-30 13:18:40 +0200

    tmux is now not opened anymore by default
    [nixos-generation: 87] [hostname: desktop]

--- common/user/bash.nix
+++ common/user/bash.nix
@@ -11,12 +11,6 @@
     enable = true;
     bashrcExtra = ''
       eval "$(oh-my-posh init bash)"
-      if [ -z "$TMUX" ] && [ '$'{UID} != 0 ]
-      then
-          tmux new-session -A -d -s main;
-          tmux send 'fastfetch' ENTER;
-          tmux a;
-      fi
     '';
   };
 }
