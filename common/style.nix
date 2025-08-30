8a42e8a728d4f0abc8e6601c87c700d67ea2cfe1
SimonReilich <simon.reilich@proton.me>
2025-08-30 13:15:14 +0200

    changed theme and wallpaper
    [nixos-generation: 85] [hostname: desktop]

--- common/style.nix
+++ common/style.nix
@@ -1,11 +1,8 @@
 { pkgs, ... }:
 {
   stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-  stylix.image = pkgs.fetchurl {
-    url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-    hash = "sha256-enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-  };
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+  stylix.image = ./wallpaper.jpg;
   stylix.polarity = "dark";
 
   stylix.fonts = {
