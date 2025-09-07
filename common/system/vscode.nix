178f5f6cbbff4ebcc9c4e5c4dbdf3ac183620a96
SimonReilich <simon.reilich@proton.me>
2025-09-07 12:01:05 +0200

    adding extension to vscode for astro support
    [nixos-generation: 93] [hostname: desktop]

--- common/system/vscode.nix
+++ common/system/vscode.nix
@@ -4,6 +4,7 @@
   users.users.simonr.packages = with pkgs; [
     (vscode-with-extensions.override {
       vscodeExtensions = with vscode-extensions; [
+        astro-build.astro-vscode
         bbenoist.nix
         brettm12345.nixfmt-vscode
         myriad-dreamin.tinymist
