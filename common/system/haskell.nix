cf1f8b57590a729ec1ad218dc7a20af8156a01ad
SimonReilich <simon.reilich@proton.me>
2025-09-17 14:29:39 +0200

    adding commonly used haskell-packages
    [nixos-generation: 108] [hostname: desktop]

--- common/system/haskell.nix
+++ common/system/haskell.nix
@@ -5,5 +5,15 @@
     cabal-install
     ghc
     haskell-language-server
+
+    haskellPackages.bytestring_0_12_2_0
+    haskellPackages.network
+    haskellPackages.monad-logger
+    haskellPackages.snap
+    haskellPackages.containers_0_8
+    haskellPackages.test-framework
+    haskellPackages.vector
+    haskellPackages.time_1_14
+    haskellPackages.mwc-random
   ];
 }
