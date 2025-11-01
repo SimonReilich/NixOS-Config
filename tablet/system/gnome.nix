634156aedbef878ce412783fa6401db470452826
SimonReilich <simon.reilich@proton.me>
2025-11-01 17:13:47 +0100

    resolving evaluation warning
    [nixos-generation: 87]
    [hostname: tablet]

--- tablet/system/gnome.nix
+++ tablet/system/gnome.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
+  services.desktopManager.gnome.extraGSettingsOverrides = ''
     [org.gnome.desktop.peripherals.touchpad]
     click-method='default'
   '';
