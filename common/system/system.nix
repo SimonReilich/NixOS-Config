67d915153e1adfe70a7146662fb11e89054232c2
SimonReilich <simon.reilich@proton.me>
2026-01-03 21:42:04 +0100

    switching to periodically checking for updates
    [nixos-generation = 288]
    [hostname: desktop]

--- common/system/system.nix
+++ common/system/system.nix
@@ -1,76 +1,83 @@
 { config, pkgs, ... }:
 
 {
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.allowReboot = true;
-  system.autoUpgrade.channel = "https://channels.nixos.org/nixos-unstable";
-
-  systemd.services.wait-for-dns = {
-    wantedBy = [ "multi-user.target" ];
-    description = "Wait for DNS to come up using 'host'";
-    restartIfChanged = false;
-    onSuccess = [ "pull-updates.service" ];
-    path = [
-      pkgs.host
-    ];
-    script = ''
-      until host github.com; do sleep 60; done
-    '';
-    serviceConfig = {
-      PassEnvironment = "DISPLAY";
-      WorkingDirectory = "/home/simonr/.dotfiles";
-      User = "simonr";
-      Type = "oneshot";
+  systemd.timers."check-dns-availability" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "15m";
+      OnUnitActiveSec = "15m";
+      Unit = "check-dns-availability.service";
     };
   };
 
-  systemd.services.pull-updates = {
-    description = "Pulls changes to system config";
-    restartIfChanged = false;
-    onSuccess = [ "rebuild.service" ];
-    onFailure = [ "trigger-user-notify.service" ];
-    path = [
-      pkgs.git
-      pkgs.openssh
-    ];
-    script = ''
-      test "$(git branch --show-current)" = "main"
-      if git diff HEAD..origin/main --name-only | grep flake.lock; then
-        exit 1
-      else
-        git pull --ff-only
-        exit 0
-      fi
-    '';
-    serviceConfig = {
-      PassEnvironment = "DISPLAY";
-      WorkingDirectory = "/home/simonr/.dotfiles";
-      User = "simonr";
-      Type = "oneshot";
+  systemd.services = {
+    check-dns-availability = {
+      wantedBy = [ "multi-user.target" ];
+      description = "Wait for DNS to come up using 'host'";
+      restartIfChanged = false;
+      onSuccess = [ "pull-updates.service" ];
+      path = [
+        pkgs.host
+      ];
+      script = ''
+        until host github.com; do sleep 60; done
+      '';
+      serviceConfig = {
+        PassEnvironment = "DISPLAY";
+        WorkingDirectory = "/home/simonr/.dotfiles";
+        User = "simonr";
+        Type = "oneshot";
+      };
     };
-  };
 
-  systemd.services.rebuild = {
-    description = "Rebuilds and activates system config";
-    restartIfChanged = false;
-    path = [
-      pkgs.nixos-rebuild
-      pkgs.systemd
-    ];
-    script = ''
-      nixos-rebuild switch --flake .
-    '';
-    serviceConfig = {
-      PassEnvironment = "DISPLAY";
-      WorkingDirectory = "/home/simonr/.dotfiles";
-      User = "root";
-      Type = "oneshot";
+    pull-updates = {
+      description = "Pulls changes to system config";
+      restartIfChanged = false;
+      onSuccess = [ "rebuild.service" ];
+      onFailure = [ "trigger-user-notify.service" ];
+      path = [
+        pkgs.git
+        pkgs.openssh
+      ];
+      script = ''
+        test "$(git branch --show-current)" = "main"
+        if git diff HEAD..origin/main --name-only | grep flake.lock; then
+          exit 1
+        else
+          git pull --ff-only
+          exit 0
+        fi
+      '';
+      serviceConfig = {
+        PassEnvironment = "DISPLAY";
+        WorkingDirectory = "/home/simonr/.dotfiles";
+        User = "simonr";
+        Type = "oneshot";
+      };
     };
-  };
 
-  systemd.services.trigger-user-notify = {
-    script = "${pkgs.systemd}/bin/systemctl --machine=simonr@.host --user start notify-change.service";
-    serviceConfig.Type = "oneshot";
+    rebuild = {
+      description = "Rebuilds and activates system config";
+      restartIfChanged = false;
+      path = [
+        pkgs.nixos-rebuild
+        pkgs.systemd
+      ];
+      script = ''
+        nixos-rebuild switch --flake .
+      '';
+      serviceConfig = {
+        PassEnvironment = "DISPLAY";
+        WorkingDirectory = "/home/simonr/.dotfiles";
+        User = "root";
+        Type = "oneshot";
+      };
+    };
+
+    trigger-user-notify = {
+      script = "${pkgs.systemd}/bin/systemctl --machine=simonr@.host --user start notify-change.service";
+      serviceConfig.Type = "oneshot";
+    };
   };
 
   systemd.user.services.notify-change = {
