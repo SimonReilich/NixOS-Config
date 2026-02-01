8df4a056bfe94a2576eefefe123036fe270be206
SimonReilich <simon.reilich@proton.me>
2026-02-01 19:26:02 +0100

    adding camera on tablet
    [nixos-generation: 187]
    [hostname: tablet]

--- tablet/system/packages.nix
+++ tablet/system/packages.nix
@@ -1,15 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    # System
     libwacom-surface
     linux-firmware
     mkcert
+    snapshot
   ];
 }
