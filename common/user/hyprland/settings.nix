b77a60b6752fb80c4f51278554cf9cf4b4f5564a
SimonReilich <simon.reilich@proton.me>
2026-02-22 22:45:56 +0100

    basic swaync config, but not final
    [nixos-generation = 1019]
    [hostname: desktop]
    
    [nixos-generation = 1020]
    [hostname: desktop]

--- common/user/hyprland/settings.nix
+++ common/user/hyprland/settings.nix
@@ -25,7 +25,12 @@
         rounding = 16;
         rounding_power = 2.0;
 
-        blur.enabled = false;
+        blur = {
+          enabled = true;
+          size = 8;
+          passes = 3;
+          new_optimizations = true;
+        };
         dim_inactive = true;
         dim_strength = 0.2;
 
@@ -51,6 +56,13 @@
         ];
         workspace_wraparound = true;
       };
+
+      layerrule = [
+        "blur on, match:namespace swaync-control-center"
+        "blur on, match:namespace swaync-notification-window"
+        "ignore_alpha 0.5, match:namespace swaync-control-center"
+        "ignore_alpha 0.5, match:namespace swaync-notification-window"
+      ];
     };
   };
 }
