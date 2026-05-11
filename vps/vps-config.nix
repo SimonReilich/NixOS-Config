a21f8b8dfbc41d16b654e3d41c61d03d23dc62dd
SimonReilich <simon.reilich@proton.me>
2026-05-11 15:43:52 +0200

    removing startup script

--- vps/vps-config.nix
+++ vps/vps-config.nix
@@ -13,16 +13,4 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF5CsQlVSAIkJbk+8jj0sppxijKw02U7K21eVTNv36D7 simon.reilich137@gmail.com"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/9Vh3mp1SoF9xzbR8BrLaSZEjx26envKfvbLYU/OO9 simon.reilich137@gmail.com"
   ];
-
-  # Setup
-  systemd.services.setup = rec {
-    wantedBy = [ "basic.target" ];
-    after = wantedBy;
-    serviceConfig = {
-      Type = "oneshot";
-      ConditionPathExists = "/etc/nixos/setup.sh";
-      ExecStart = "/etc/nixos/setup.sh";
-      ExecStartPost = "${pkgs.coreutils}/bin/rm /etc/nixos/setup.sh";
-    };
-  };
 }
